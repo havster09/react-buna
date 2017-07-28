76c71832e4d8612b2171a890fa3ed2d6d7920a56
haven <haven@actio.com.au>
2017-07-28 14:01:48 +1000

    build prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,43 @@
 const path = require('path');
+const webpack = require('webpack');
 
 const config = {
-  entry: ['babel-polyfill', './lib/renderers/dom.js'],
+  // entry: ['babel-polyfill', './lib/renderers/dom.js'],
+  entry: {
+    app: ['./lib/renderers/dom.js'],
+    vendor: [
+      'babel-polyfill',
+      'react',
+      'react-dom',
+      'prop-types',
+      'axios',
+      'lodash.debounce',
+      'lodash.pickby'
+    ]
+  },
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' }
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'env', 'stage-2']
+          }
+        }
+      }
     ]
-  }
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    })
+  ]
 };
 
 module.exports = config;
